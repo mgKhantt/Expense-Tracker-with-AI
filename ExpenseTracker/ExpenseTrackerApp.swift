4997b3a1692024ce078d0a13f9072148dc3c03f5
mgKhantt <khantphonenaing156@gmail.com>
2026-03-19 10:16:26 +0630

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Khant Phone Naing  on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
